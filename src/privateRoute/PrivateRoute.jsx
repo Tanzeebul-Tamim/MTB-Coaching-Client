1a1bfe3b278a3aad85c3f731d16c9220c926a366
Tanzeebul Tamim <tamim200091@gmail.com>
2025-06-04 03:09:54 +0600

    refactor: Refactor PrivateRoute for cleaner logic and formatting consistency

--- src/privateRoute/PrivateRoute.jsx
+++ src/privateRoute/PrivateRoute.jsx
@@ -1,26 +1,34 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { AuthContext } from "../providers/AuthProvider";
-import { PropagateLoader  } from "react-spinners";
+import { PropagateLoader } from "react-spinners";
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
     const { loading, user } = useContext(AuthContext);
     const location = useLocation();
 
-    if(loading) {
+    if (loading) {
         return (
-            <div style={{height: "700px"}} className="flex justify-center items-center">
-                <PropagateLoader  color="rgb(234 179 8)" />
+            <div
+                style={{ height: "700px" }}
+                className="flex justify-center items-center"
+            >
+                <PropagateLoader color="rgb(234 179 8)" />
             </div>
         );
     }
 
-    if(user) {
-        return children;
+    if (!user) {
+        return (
+            <Navigate
+                state={{ from: location, showToast: true }}
+                to="/login"
+            ></Navigate>
+        );
     }
 
-    return <Navigate state={{from: location, showToast: true}} to="/login"></Navigate>
+    return children;
 };
 
 export default PrivateRoute;
