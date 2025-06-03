b956268850073b54c205e9e54df71d56f06373cf
Tanzeebul Tamim <tamim200091@gmail.com>
2025-06-04 01:14:01 +0600

    feat: add <RouteTracker /> to Main layout for route tracking

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../shared_components/Navbar/Navbar";
 import Footer from "../shared_components/Footer/Footer";
 import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import RouteTracker from "../RouteTracker";
 
 const Main = () => {
     return (
         <div className="overflow-x-hidden">
+            <RouteTracker />
             <ScrollToTop></ScrollToTop>
             <Navbar></Navbar>
             <Outlet location2={location.pathname} ></Outlet>
