7c3e905826dd349fbfbc15470c1ddbf66df6ddb1
Tanzeebul Tamim <tamim200091@gmail.com>
2025-06-04 03:12:54 +0600

    Add utility function to enable smooth scrolling with vertical offset

---
+++ src/ScrollToTop/ScrollWithOffset.js
@@ -0,0 +1,5 @@
+export default function scrollWithOffset(el) {
+    const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+    const yOffset = -100;
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: "smooth" });
+}
