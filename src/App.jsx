13b4279c8bb6c040e53407247c368059c3c79455
Tanzeebul Tamim <tamim200091@gmail.com>
2025-06-14 11:43:45 +0600

    feat(ui): add custom ToastContainer with theme-aware styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import AuthProvider from "./providers/AuthProvider";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ScreenSizeProvider from "./providers/ScreenSizeProvider";
 import NetworkStatusProvider from "./providers/NetworkStatusProvider";
 import router from "./routes/config/router";
+import Toast from "./components/ui/Toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
@@ -15,20 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <ScreenSizeProvider>
                 <NetworkStatusProvider>
                     <RouterProvider router={router} />
-                    <ToastContainer
-                        className=""
-                        position="top-center"
-                        autoClose={5000}
-                        hideProgressBar={false}
-                        newestOnTop={false}
-                        closeOnClick
-                        limit={2}
-                        rtl={false}
-                        pauseOnFocusLoss
-                        draggable
-                        pauseOnHover
-                        theme="dark"
-                    />
+                    <Toast />
                 </NetworkStatusProvider>
             </ScreenSizeProvider>
         </AuthProvider>
---
+++ src/components/ui/Toast.jsx
@@ -0,0 +1,25 @@
+import { ToastContainer } from "react-toastify";
+import useDarkTheme from "../../hooks/useDarkTheme";
+
+const Toast = () => {
+    const isDarkTheme = useDarkTheme();
+
+    return (
+        <ToastContainer
+            className=""
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            limit={2}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme={isDarkTheme ? "dark" : "light"}
+        />
+    );
+};
+
+export default Toast;
