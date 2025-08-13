f90f4c2f06e7aebbcb344770785e5f8d18d228fa
Tanzeebul Tamim <tamim200091@gmail.com>
2025-08-13 22:08:51 +0600

    feat(layout/map): hide Footer on auth routes & improve LeafletMap responsiveness
    - use useLocation to detect auth pages and conditionally hide Footer
    - add `smallDevice` prop and refine Google Maps button/layout for small screens

--- src/components/layout/Footer/LeafletMap/LeafletMap.jsx
+++ src/components/layout/Footer/LeafletMap/LeafletMap.jsx
@@ -13,19 +13,25 @@ L.Icon.Default.mergeOptions({
     shadowUrl: "/assets/marker-shadow.png",
 });
 
-const LeafletMap = () => {
+const LeafletMap = ({ smallDevice }) => {
     const center = { lat: 51.97305763844625, lng: 5.556715414769293 };
     const ZOOM_LEVEL = 15.5;
     const mapRef = useRef();
 
     return (
-        <div className="relative group w-full h-full">
-            <div className="absolute right-0 top-2 z-[9999] opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+        <div
+            className={`relative group w-full h-full ${smallDevice && "mt-4"}`}
+        >
+            <div className="absolute z-[9999] transition-all duration-300 lg:opacity-0 lg:group-hover:opacity-100 right-0 lg:top-2 -top-2 lg:hover:scale-105 lg:hover:right-1 lg:hover:top-2 ease-in-out">
                 <a
                     href="https://www.google.com/maps/place/MTB+route,+rhenen/@51.972747,5.5471453,16z/data=!4m10!1m2!2m1!1smountain+bike+trail!3m6!1s0x47c654097f4e5ef1:0x2691c597450a599b!8m2!3d51.972747!4d5.5566725!15sChNtb3VudGFpbiBiaWtlIHRyYWlsWhUiE21vdW50YWluIGJpa2UgdHJhaWySAQ5zcG9ydHNfY29tcGxleJoBJENoZERTVWhOTUc5blMwVkpRMEZuU1VNMWR6UlRhRFpCUlJBQqoBaAoIL20vMDE5OWcKCC9tLzBfbWpwEAEqFyITbW91bnRhaW4gYmlrZSB0cmFpbCgAMh4QASIaVSV2fnus8nDHm1tfIzap4DhCiohlFPsxWzwyFxACIhNtb3VudGFpbiBiaWtlIHRyYWls4AEA-gEECAAQGg!16s%2Fg%2F11dybfjq3d?entry=ttu&g_ep=EgoyMDI1MDczMC4wIKXMDSoASAFQAw%3D%3D"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="dark:bg-primary bg-secondary dark:text-yellow-900 text-orange-900 px-3 py-1 rounded-lg shadow-md hover:bg-primary hover:text-white lg:text-sm text-xs font-bold"
+                    className={`bg-secondary dark:text-yellow-900 text-orange-900 px-3 ${
+                        smallDevice
+                            ? "rounded-md"
+                            : "lg:rounded-bl-lg lg:rounded-tr-lg"
+                    } py-1 rounded-s- shadow-md hover:bg-primary lg:text-sm text-xs font-bold`}
                 >
                     View on Google Maps
                 </a>
